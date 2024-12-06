f8f6d74cbb13aa267e3d5abe20e95fde152c7183
saadkhan4 <saadshaheen1327@gmail.com>
2024-12-06 21:54:34 +0500

    shopping cart form using flask

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask , render_template ,request
+from flask import Flask, redirect , render_template ,request, session 
+from flask_session import Session 
+
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False   # as soon as you quit the browser your session will be deleted.
+app.config["SESSION_TYPE"] = "filesystem" # contents of your shopping cart stores in servers.
+Session(app)
+
 @app.route("/")
 def index():
-    name = request.args.get("name" , "world")
-    return  render_template("index.html" , name=name)
+   return render_template("index.html", name=session.get("name"))
+
+
+@app.route("/login" , methods=["GET" , "POST"])
+def login():
+   if request.method == "POST":
+      session["name"] = request.form.get("name")
+      return redirect("/") 
+   return render_template("login.html")
