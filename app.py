ffb52a58d86db476e5541b297e33e22b05377ca3
saadkhan4 <saadshaheen1327@gmail.com>
2024-12-07 19:29:04 +0500

    shopping cart form using flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_session import Session
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False   # as soon as you quit the browser your session will be deleted.
-app.config["SESSION_TYPE"] = "filesystem" # contents of your shopping cart stores in servers.
+app.config["SESSION_TYPE"] = "filesystem" # contents of your shopping cart stores in servers & stays there.
 Session(app)
 
 @app.route("/")
