170cdf0656922527edba57aeabef4e7c6c11bf5e
saadkhan4 <saadshaheen1327@gmail.com>
2024-12-08 23:22:02 +0500

    shopping cart form using flask

--- app.py
+++ app.py
@@ -19,3 +19,10 @@ def login():
       session["name"] = request.form.get("name")
       return redirect("/") 
    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+   session.clear()
+   return redirect("/")
+
